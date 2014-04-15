e38ab4e92e6b915267ce93433b40f452c24471b0
Gary Johnson <gary@gjtt.com>
2014-04-14 21:43:33 -0400

    FudgeTestCase, on tearDown, should clear expectations

--- tests/fakery/fudge_test_case_tests.py
+++ tests/fakery/fudge_test_case_tests.py
@@ -4,59 +4,66 @@ from fakery.fudge_test_case import FudgeTestCase
 
 class FudgeTestCaseTest(TestCase):
 
+    class MyTest(FudgeTestCase):
+        def test_foo(self):
+            pass
+        def tearDown(self):
+            pass
+
     def tearDown(self):
         fudge.clear_expectations()
 
     @fudge.with_fakes
     def test_patches_test_method_to_clear_calls(self):
-        clear_calls_mock = fudge.Fake('clear_calls').is_callable()
-        clear_calls_patch = fudge.patch_object('fudge', 'clear_calls', clear_calls_mock)
-        verify_patch = fudge.patch_object('fudge', 'verify', fudge.Fake('verify').is_callable())
-        class MyTest(FudgeTestCase):
-            def test_foo(self):
-                pass
+        self.setup_fudge_mocks()
 
-        clear_calls_mock.expects_call()
+        self.clear_calls_mock.expects_call()
 
-        test_case = MyTest('test_foo')
+        test_case = FudgeTestCaseTest.MyTest('test_foo')
         test_case.test_foo()
-
-        clear_calls_patch.restore()
-        verify_patch.restore()
+        self.restore_fudge_patches()
 
     @fudge.with_fakes
     def test_patches_test_method_to_verify(self):
-        clear_calls_patch = fudge.patch_object('fudge', 'clear_calls', fudge.Fake('clear_calls').is_callable())
-        verify_mock = fudge.Fake('verify').is_callable()
-        verify_patch = fudge.patch_object('fudge', 'verify', verify_mock)
-        class MyTest(FudgeTestCase):
-            def test_foo(self):
-                pass
+        self.setup_fudge_mocks()
 
-        verify_mock.expects_call()
+        self.verify_mock.expects_call()
 
-        test_case = MyTest('test_foo')
+        test_case = FudgeTestCaseTest.MyTest('test_foo')
         test_case.test_foo()
+        self.restore_fudge_patches()
 
-        clear_calls_patch.restore()
-        verify_patch.restore()
+    @fudge.with_fakes
+    def test_clears_expectations_in_tear_down(self):
+        self.setup_fudge_mocks()
+
+        self.clear_expectations_mock.expects_call()
+
+        test_case = FudgeTestCaseTest.MyTest('test_foo')
+        test_case.tearDown()
+        self.restore_fudge_patches()
 
     @fudge.with_fakes
     def test_patches_test_method_to_clear_calls_only_once(self):
-        clear_calls_mock = fudge.Fake('clear_calls').is_callable()
-        clear_calls_patch = fudge.patch_object('fudge', 'clear_calls', clear_calls_mock)
-        verify_mock = fudge.Fake('verify').is_callable()
-        verify_patch = fudge.patch_object('fudge', 'verify', verify_mock)
-        class MyTest(FudgeTestCase):
-            def test_foo(self):
-                pass
+        self.setup_fudge_mocks()
 
-        verify_mock.expects_call().times_called(1)
-        clear_calls_mock.expects_call().times_called(1)
+        self.verify_mock.expects_call().times_called(1)
+        self.clear_calls_mock.expects_call().times_called(1)
 
-        test_case = MyTest('test_foo')
-        test_case = MyTest('test_foo')
+        test_case = FudgeTestCaseTest.MyTest('test_foo')
+        test_case = FudgeTestCaseTest.MyTest('test_foo')
         test_case.test_foo()
+        self.restore_fudge_patches()
 
-        clear_calls_patch.restore()
-        verify_patch.restore()
+    def setup_fudge_mocks(self):
+        self.clear_expectations_mock = fudge.Fake('clear_expectations').is_callable()
+        self.clear_expectations_patch = fudge.patch_object('fudge', 'clear_expectations', self.clear_expectations_mock)
+        self.clear_calls_mock = fudge.Fake('clear_calls').is_callable()
+        self.clear_calls_patch = fudge.patch_object('fudge', 'clear_calls', self.clear_calls_mock)
+        self.verify_mock = fudge.Fake('verify').is_callable()
+        self.verify_patch = fudge.patch_object('fudge', 'verify', self.verify_mock)
+
+    def restore_fudge_patches(self):
+        self.clear_expectations_patch.restore()
+        self.clear_calls_patch.restore()
+        self.verify_patch.restore()
