fc50a08cce3661c6a4ee1e1b76b13220ef243106
Gary Johnson <gary@gjtt.com>
2014-04-14 22:09:18 -0400

    v0.1.0

--- setup.py
+++ setup.py
@@ -9,18 +9,12 @@ from setuptools import setup
 
 setup(
     name='fakery',
-    version='0.1',
+    version='0.1.0',
     author='Gary Johnson',
     author_email = 'gary@gjtt.com',
-    description = '',
-    install_requires=['fudge'],
-    tests_require=['fudge'],
+    description = 'Trying to make fudge mocks in Python easier.',
+    install_requires=['fudge', 'nose'],
+    tests_require=['fudge', 'nose'],
     license = 'MIT License',
     py_modules = ['fakery'],
-    entry_points = {
-        'nose.plugins.0.10': [
-            'fakery = fakery:Fakery'
-            ]
-        }
-
     )
